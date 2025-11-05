70f094d898d3b757d3c5d24dd7541783d99f0f7a
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-05 11:50:28 +0530

    backend done

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,10 @@ import cookieParser from "cookie-parser";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.routes.js"
 import companyRoute from "./routes/company.routes.js"
+import jobRoute from "./routes/job.routes.js"
+import applicationRoute from "./routes/application.routes.js"
+import "./models/application.model.js";
+
 
 dotenv.config({})
 const app = express();
@@ -25,8 +29,8 @@ const PORT = process.env.PORT || 5001
 // api's
 app.use("/api/users", userRoute);
 app.use("/api/company", companyRoute);
-// app.use("/api/job", jobRoute);
-// app.use("/api/application", applicationRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
 
 app.listen(PORT, () => {
