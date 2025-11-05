6c5f180adb68d0c0c995be64845ade36ef7fcf6f
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-05 08:47:40 +0530

    start

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import connectDB from "./utils/db";
+
+dotenv.config({})
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser)
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions))
+
+const PORT = process.env.PORT || 5001
+
+// api's
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
+
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`✅ Server is running on port ${PORT}`);
+});
